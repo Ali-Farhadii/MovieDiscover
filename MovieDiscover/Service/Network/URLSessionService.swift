999712d8e2075a645b8be3266b7f8cc1c81b4f05
Ali Farhadi <ali.farhadi@snapp.cab>
2024-05-01 09:28:47 +0330

    Add URLSession service

--- MovieDiscover/Service/Network/URLSessionService.swift
+++ MovieDiscover/Service/Network/URLSessionService.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct URLSessionService: NetworkService {
+    var urlSession = URLSession.shared
+    
+    func request<T: Decodable>(with endPoint: Endpoint) async throws -> T {
+        guard let urlRequest = endPoint.createURLRequest() else { throw NetworkError.badRequest }
+        let (data, response) = try await urlSession.data(for: urlRequest)
+        
+        guard let response = response as? HTTPURLResponse,
+              response.statusCode == 200 else { throw NetworkError.invalidResponse }
+        
+        if let decodedData: T = decodeData(data) {
+            return decodedData
+        } else {
+            throw NetworkError.decodeFailed
+        }
+    }
+    
+    func decodeData<T: Decodable>(_ data: Data) -> T? {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .secondsSince1970
+        return try? decoder.decode(T.self, from: data)
+    }
+}
