77bb05bf08c8038b728ef9261736d09031c5119c
Ali Farhadi <ali.farhadi@snapp.cab>
2024-05-01 09:16:52 +0330

    Add Endpoint protocol

--- MovieDiscover/Service/Network/Endpoint.swift
+++ MovieDiscover/Service/Network/Endpoint.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+protocol Endpoint {
+    var path: String { get }
+    var httpMethod: HTTPMethod { get }
+    var httpHeader: [String: String]? { get }
+    var allHeaders: [String: String] { get }
+    var httpBody: Encodable? { get }
+}
+
+extension Endpoint {
+    var allHeaders: [String: String] {
+        defaultHeader.merging(httpHeader ?? [:]) { (_, new) in new }
+    }
+    
+    private var defaultHeader: [String: String] {
+        [
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        ]
+    }
+    
+    func createURLRequest() -> URLRequest? {
+        guard let url = URL(string: Constants.baseURL + path) else { return nil }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = httpMethod.rawValue
+        
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        if let body = httpBody {
+            let jsonData = try? encoder.encode(body)
+            urlRequest.httpBody = jsonData
+        }
+        
+        allHeaders.forEach {
+            urlRequest.addValue($1, forHTTPHeaderField: $0)
+        }
+        
+        return urlRequest
+    }
+}
