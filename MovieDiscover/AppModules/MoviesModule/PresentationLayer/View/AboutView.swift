d31cdcebc8e1165d5e609ae11ad595e6fc9cbc8c
Ali Farhadi <ali.farhadi@snapp.cab>
2024-05-01 10:00:26 +0330

    Add About screen view

---
+++ MovieDiscover/AppModules/MoviesModule/PresentationLayer/View/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  MovieDiscover
+//
+//  Created by Ali Farhadi on 5/1/24.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
