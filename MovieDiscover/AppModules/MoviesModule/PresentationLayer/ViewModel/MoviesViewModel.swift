b688a6494b6ed865837ec0cea8c6f98a807705f7
Ali Farhadi <ali.farhadi@snapp.cab>
2024-05-01 11:52:01 +0330

    Add movies viewModel

--- MovieDiscover/AppModules/MoviesModule/PresentationLayer/ViewModel/MoviesViewModel.swift
+++ MovieDiscover/AppModules/MoviesModule/PresentationLayer/ViewModel/MoviesViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+class MoviesViewModel: ObservableObject {
+    
+    @Published var movies: [MoviePresentationModel] = []
+    @Published var isLoading = false
+    @Published var isError = false
+    
+    let repository: MoviesRepositoryProtocol
+    var page = 1
+    var totalPage = 0
+    
+    init(repository: MoviesRepositoryProtocol) {
+        self.repository = repository
+    }
+
+    @MainActor func fetchWithLoading() {
+        isLoading = true
+        Task {
+            await fetchMovies()
+            isLoading = false
+        }
+    }
+    
+    @MainActor
+    func fetchMovies() async {
+        do {
+            let discoveryBusinessModel = try await repository.fetchMovies()
+            movies = discoveryBusinessModel.results.map(mapMoviesToPresentationModel(_:))
+            totalPage = discoveryBusinessModel.totalPages
+        } catch {
+            isError = true
+        }
+    }
+    
+    private func mapMoviesToPresentationModel(_ businessModel: MovieBusinessModel) -> MoviePresentationModel {
+        MoviePresentationModel(title: "", overview: "",
+                               posterPath: "", releaseDate: "")
+    }
+}
