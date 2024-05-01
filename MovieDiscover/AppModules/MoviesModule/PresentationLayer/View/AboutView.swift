1419e8b76ffe7542d73ed1aedc94cc62633da41b
Ali Farhadi <ali.farhadi@snapp.cab>
2024-05-01 13:53:08 +0330

    Complete About screen view

--- MovieDiscover/AppModules/MoviesModule/PresentationLayer/View/AboutView.swift
+++ MovieDiscover/AppModules/MoviesModule/PresentationLayer/View/AboutView.swift
@@ -10,8 +10,47 @@ import SwiftUI
 struct AboutView: View {
     var body: some View {
         NavigationStack {
-            Text("Hello, World!")
-                .navigationTitle("About us")
+            VStack(alignment: .center) {
+                Image(.ali)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .clipShape(.capsule)
+                
+                Spacer()
+                    .frame(height: 30)
+                
+                Text("Ali Farhadi")
+                    .font(.largeTitle)
+                
+                Text("iOS Developer")
+                    .font(.title)
+                
+                Spacer()
+                    .frame(height: 50)
+                
+                HStack {
+                    Text("See my LinkedIn page:")
+                    
+                    Link("LinkedIn", 
+                         destination: URL(string: "https://www.linkedin.com/in/ali-farhadi/")!)
+                }
+                .font(.headline)
+                
+                Spacer()
+                    .frame(height: 30)
+                
+                HStack {
+                    Text("See my GitHub page:")
+                    
+                    Link("Github",
+                         destination: URL(string: "https://www.github.com/Ali-Farhadii")!)
+                }
+                .font(.headline)
+                
+                Spacer()
+            }
+                .navigationTitle("About me")
         }
     }
 }
