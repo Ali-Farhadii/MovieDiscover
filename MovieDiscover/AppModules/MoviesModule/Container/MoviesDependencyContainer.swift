76a59325b8c95ca9a37875858beb1babb7771b89
Ali Farhadi <ali.farhadi@snapp.cab>
2024-05-01 09:58:52 +0330

    Add dependency container for Movies module

---
+++ MovieDiscover/AppModules/MoviesModule/Container/MoviesDependencyContainer.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesDependencyContainer.swift
+//  MovieDiscover
+//
+//  Created by Ali Farhadi on 5/1/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct MoviesDependencyContainer {
+    
+    var movieView: some View {
+        MoviesView()
+    }
+}
