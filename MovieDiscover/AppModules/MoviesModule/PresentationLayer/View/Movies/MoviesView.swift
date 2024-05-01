303294a050b12289ad5738ec5359971b8090c598
Ali Farhadi <ali.farhadi@snapp.cab>
2024-05-01 11:52:34 +0330

    Base structure of Movies screen view

--- MovieDiscover/AppModules/MoviesModule/PresentationLayer/View/Movies/MoviesView.swift
+++ MovieDiscover/AppModules/MoviesModule/PresentationLayer/View/Movies/MoviesView.swift
@@ -8,20 +8,57 @@
 import SwiftUI
 
 struct MoviesView: View {
+    
+    @ObservedObject var viewModel: MoviesViewModel
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+            if viewModel.isLoading {
+                loadingView
+            } else if viewModel.isError {
+                errorView
+            } else {
+                moviesListView
             }
+        }
+        .refreshable {
+            await viewModel.fetchMovies()
+        }
+        .task {
+            viewModel.fetchWithLoading()
+        }
+    }
+    
+    var loadingView: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle())
             .padding()
             .navigationTitle("Movies")
+    }
+    
+    var errorView: some View {
+        VStack {
+            Text("Error occurred while loading Data")
+                .foregroundColor(.red)
+                .padding()
+            Button {
+                viewModel.fetchWithLoading()
+            } label: {
+                Text(NSLocalizedString("Try Again Button Title", value: "Try Again", comment: ""))
+                    .foregroundColor(.blue)
+            }
         }
+        .navigationTitle("Movies")
+    }
+    
+    var moviesListView: some View {
+        List(viewModel.movies) { movie in
+            Text("Test")
+        }
+        .navigationTitle("Movies")
     }
 }
 
 #Preview {
-    MoviesView()
+    MoviesDependencyContainer().movieView
 }
