6e8577234905f1459f69e64c4c77871486115aec
Ali Farhadi <ali.farhadi@snapp.cab>
2024-05-01 08:47:22 +0330

    Initial Commit

---
+++ MovieDiscover/MovieDiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDiscoverApp.swift
+//  MovieDiscover
+//
+//  Created by Ali Farhadi on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
