9ea52769d787e8236a136550bdb9b70ae0405903
Ali Farhadi <ali.farhadi@snapp.cab>
2024-05-01 10:07:25 +0330

    Add navigation bar and title to both views

--- MovieDiscover/AppModules/MoviesModule/PresentationLayer/View/AboutView.swift
+++ MovieDiscover/AppModules/MoviesModule/PresentationLayer/View/AboutView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Text("Hello, World!")
+                .navigationTitle("About us")
+        }
     }
 }
 
--- MovieDiscover/AppModules/MoviesModule/PresentationLayer/View/MoviesView.swift
+++ MovieDiscover/AppModules/MoviesModule/PresentationLayer/View/MoviesView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct MoviesView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+            .navigationTitle("Movies")
         }
-        .padding()
     }
 }
 
